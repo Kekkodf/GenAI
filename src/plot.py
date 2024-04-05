1b00c85dac917c98fdaba043a8a63aa4c868dd0e
kdf_7 <francesco.defaveri1999@gmail.com>
2024-04-05 17:45:07 +0200

    updating data and plot

--- src/plot.py
+++ src/plot.py
@@ -10,16 +10,18 @@ realResponses = pd.read_csv('data/obfuscated/obfuscatedTestRun_CMP_5.csv')
 # Keep only unique ids
 realResponses = realResponses.drop_duplicates(subset='question').reset_index(drop=True)
 
-eps = [5, 10, 20, 50]
+eps = [5, 10, 20, 30, 40, 50]
 models = ['llama2-70b-4096', 'mixtral-8x7b-32768', 'gemma-7b-it']
 
 # Initialize a dictionary to store average precision values for each model and mechanism
 avg_precision_cmp = {model: [] for model in models}
 avg_precision_mahalanobis = {model: [] for model in models}
 
+df = pd.DataFrame(columns=["model", "mechanism", "epsilon", "precision"])
 for model in models:
     for e in eps:
-        path_cmp = f'output/{model}/CMP_{e}.csv'
+        mechanism = 'CMP'
+        path_cmp = f'output/{model}/{mechanism}/{mechanism}_{e}.csv'
         df_cmp = pd.read_csv(path_cmp)
         responses_cmp = df_cmp['response']
         # Compare the responses with the real responses for CMP mechanism
@@ -29,7 +31,17 @@ for model in models:
         precision_cmp = realResponses['correct_cmp'].mean()
         avg_precision_cmp[model].append(precision_cmp)
 
-        path_mahalanobis = f'output/{model}/Mahalanobis_{e}.csv'
+        temp = pd.DataFrame({
+            "model": [model],
+            "mechanism": [mechanism],
+            "epsilon": [e],
+            "precision": [precision_cmp]
+        })
+        # Append the results to the dataframe
+        df = pd.concat([df, temp], ignore_index=True)
+
+        mechanism = 'Mahalanobis'
+        path_mahalanobis = f'output/{model}/{mechanism}/{mechanism}_{e}.csv'
         df_mahalanobis = pd.read_csv(path_mahalanobis)
         responses_mahalanobis = df_mahalanobis['response']
         # Compare the responses with the real responses for Mahalanobis mechanism
@@ -39,13 +51,25 @@ for model in models:
         precision_mahalanobis = realResponses['correct_mahalanobis'].mean()
         avg_precision_mahalanobis[model].append(precision_mahalanobis)
 
-plt.figure(figsize=(12, 5))
+        temp = pd.DataFrame({
+            "model": [model],
+            "mechanism": [mechanism],
+            "epsilon": [e],
+            "precision": [precision_mahalanobis]
+        })
+        # Append the results to the dataframe
+        df = pd.concat([df, temp], ignore_index=True)
+
+# Save the results to a CSV file
+df.to_csv('output/average_precision.csv', index=False)
+
+plt.figure(figsize=(15, 8))
 colors = sns.color_palette('colorblind', n_colors=len(models))
 
 for i, model in enumerate(models):
     color = colors[i]
     if model == 'llama2-70b-4096':
-        model_label = 'LLaMA2'
+        model_label = 'Llama2'
     elif model == 'gemma-7b-it':
         model_label = 'Gemma'
     elif model == 'mixtral-8x7b-32768':
@@ -58,7 +82,7 @@ for i, model in enumerate(models):
 
 plt.xlabel(r'$\varepsilon$')
 plt.ylabel('Average Precision')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.82), fancybox=False, shadow=False, ncol=2)
 plt.tight_layout()
 plt.grid(False)
 
